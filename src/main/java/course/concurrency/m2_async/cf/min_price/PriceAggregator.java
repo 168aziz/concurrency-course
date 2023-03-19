858473a1a1ee0b3da81cff25b5d5c78f5f3af648
Tuychiyev Aziz <azizbektuychiyev99@gmail.com>
2023-03-20 01:13:13 +0500

    Module 2: 2-3 Lesson

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -1,7 +1,11 @@
 package course.concurrency.m2_async.cf.min_price;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class PriceAggregator {
 
@@ -18,7 +22,21 @@ public class PriceAggregator {
     }
 
     public double getMinPrice(long itemId) {
-        // здесь будет ваш код
-        return 0;
+        Function<Long, CompletableFuture<Double>> priceFunction =
+                id -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, id))
+                        .completeOnTimeout(Double.POSITIVE_INFINITY, 3000, TimeUnit.MILLISECONDS)
+                        .handle((res, ex) -> res != null ? res : Double.POSITIVE_INFINITY);
+
+        CompletableFuture[] completableFutures = shopIds.stream()
+                .map(priceFunction)
+                .toArray(CompletableFuture[]::new);
+
+        CompletableFuture.allOf(completableFutures).join();
+
+        return Arrays.stream(completableFutures)
+                .mapToDouble(a-> (double) a.join())
+                .filter(Double::isFinite)
+                .min()
+                .orElse(Double.NaN);
     }
 }
